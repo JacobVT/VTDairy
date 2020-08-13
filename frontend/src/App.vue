01b66baa62a454ae8d5c2e677c0cc5264ca76897
JacobVT <jacob.vantol@wsu.edu>
2020-08-12 17:25:57 -0700

    added fade transitions, refactor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -52,30 +52,43 @@
 
     <v-main>
       <v-container>
-        <v-row justify="center" align="center" class="pb-5" >
+        <!-- Title -->
+        <v-row justify="center" align="end" class="pb-5" style="height: 55vh">
           <v-col class="text-center" cols="12">
             <h5 class="f-dancing h5-size" > Welcome to the</h5>
             <h6 class="f-martel h6-size stroke" style=" color: skyblue"> Van Tol Dairy </h6>
-            <!-- <h2 class="text-h2">
-              <v-icon dark left>mdi-star</v-icon>
-              website
-              <v-icon dark right>mdi-star</v-icon>
-            </h2> -->
           </v-col>
         </v-row>
-        <v-row 
+
+        <!-- Transition -->
+        <v-row style="height: 90vh" align="start" justify="center">
+          <v-expand-x-transition>
+            <v-divider v-if="scrolled" class="divider"></v-divider>
+          </v-expand-x-transition>
+          <span
+            v-if="scrolled"
+            class="fade-in"
+          >
+            Who are we?
+          </span>
+        </v-row>
+        
+        <!-- Slide -->
+        <v-row id="slides"
         justify="center" align="center"
         v-observe-visibility="visibilityChanged">
           <v-fade-transition>
             <v-slide-group
               v-if="cardVisible"
-              v-model="slide"
+              v-model="currentSlide"
             >
              <v-slide-item
+                v-for="(slide, i) in slides"
+                :key="i"
                 v-slot:default="{ active, toggle }"
               >
                 <v-card
-                  class="ma-4"
+                  class="ma-5"
                   height="400"
                   width="300"
                   @mouseenter="toggle"
@@ -84,11 +97,11 @@
                   <v-img
                     class="white--text align-start"
                     height="400px"
-                    src="./assets/owners.jpg"
+                    :src="require('./assets/' + slide.image)"
                   >
-                    <v-card-title class="card-title-overlay f-martel">Our Story</v-card-title>
+                    <v-card-title class="card-title-overlay f-martel">{{slide.title}}</v-card-title>
                     <v-expand-transition>
-                      <v-card-text v-if="active"  class="card-title-overlay" style="height: 400px">Learn about...</v-card-text>
+                      <v-card-text v-if="active"  class="card-title-overlay" style="height: 380px">{{slide.text}}</v-card-text>
                     </v-expand-transition>
                   </v-img>
                 </v-card>
@@ -97,12 +110,29 @@
           </v-fade-transition>
         </v-row>
       </v-container>
+    <v-fab-transition>
+      <v-btn
+        v-if="!scrolled"
+        fab
+        class="thing"
+        color="rgba(0, 0, 0, 0.5)"
+        fixed
+        large
+        dark
+        bottom
+        right
+        href="#slides"
+      >
+        <v-icon>mdi-chevron-double-down</v-icon>
+      </v-btn>
+    </v-fab-transition>
 
       <!-- <div class="hideme text--black">Fade In</div>
       <div style="height: 500px"/> -->
       
       <!-- fade in text + carousel -->
       <!-- add clickable cow, add any logic and screen effects to the landing page -->
+    
     </v-main>
   </v-app>
 </template>
@@ -117,10 +147,32 @@ export default {
     //Recipes
   },
 
+  computed: {
+  },
+
   data: () => ({
+    loaded: false,
     scrolled: false,
     cardVisible: false,
-    slide: null,
+    currentSlide: null,
+    slides: [
+      {
+        title: "Read Our Story",
+        text: "Learn about...",
+        image: "owners.jpg",
+      },
+      {
+        title: "Explore Recipes",
+        text: "Learn about...",
+        image: "swing.jpg",
+      },
+      {
+        title: "Play Games",
+        text: "Learn about...",
+        image: "jump.jpg",
+      },
+    ],
+
   }),
 
   created () {
@@ -142,6 +194,15 @@ export default {
 };
 </script>
 <style scoped>
+#dairy {
+  background: rgb(255, 254, 241)
+}
+html {
+  scroll-behavior: smooth;
+}
+.divider {
+  border-width: 2px;
+}
 .f-dancing {
   font-family: 'Dancing Script', cursive;
   /* font-size: 5rem; */
@@ -162,7 +223,35 @@ export default {
 .card-title-overlay {
   background-color: rgba(0, 0, 0, 0.5);
 }
-#dairy {
-  background: rgb(255, 254, 241)
+.fade-in {
+animation: fadeIn ease 5s;
+-webkit-animation: fadeIn ease 5s;
+-moz-animation: fadeIn ease 5s;
+-o-animation: fadeIn ease 5s;
+-ms-animation: fadeIn ease 5s;
+}
+@keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-moz-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-webkit-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-o-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-ms-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
 }
 </style>
