f6d3027e1e83f07f1f792634ca99813987745819
JacobVT <jacob.vantol@wsu.edu>
2020-08-19 21:29:27 -0700

    Added placeholder images

--- frontend/src/components/Story.vue
+++ frontend/src/components/Story.vue
@@ -4,7 +4,20 @@
 			<v-row justify="center" align="center">
 				<v-col cols="6">
 					<v-card class="picture-frame rotate-1" elevation="16" tile>
-						<v-img src="../assets/brothers.jpg">
+						<v-img 
+							src="../assets/brothers.jpg"
+							lazy-src="../assets/ph_brothers.jpg"
+							style="max-width: 100%; max-height: 100%"
+						>
+							<template v-slot:placeholder>
+								<v-row
+								class="fill-height ma-0"
+								align="center"
+								justify="center"
+								>
+								<v-progress-circular indeterminate color="primary" size="96" width="8"></v-progress-circular>
+								</v-row>
+							</template>
 						</v-img>
 					</v-card>
 				</v-col>
@@ -23,25 +36,40 @@
 				</v-col>
 				<v-col cols="6">
 					<v-card class="picture-frame rotate-2" elevation="16" tile>
-						<v-img src="../assets/brownie.jpg">
+						<v-img 
+							src="../assets/brownie.jpg"
+							lazy-src="../assets/ph_brownie.jpg"
+							style="max-width: 100%; max-height: 100%"
+						>
+							<template v-slot:placeholder>
+								<v-row
+								class="fill-height ma-0"
+								align="center"
+								justify="center"
+								>
+								<v-progress-circular indeterminate color="primary" size="96" width="8"></v-progress-circular>
+								</v-row>
+							</template>
 						</v-img>
 					</v-card>
 				</v-col>
 			</v-row>
 		</v-container>
-		<v-parallax src="../assets/cows.jpg" height="800">
-			<v-container>
-				<v-row justify="center">
-					<v-col cols="6" class="card-title-overlay pa-5" dark>
-						<span class="f-dancing h5-size"> Teaming Up With Nature </span> <br/>
-						<span class="f-roboto-slab h1-size">
-							Van Tol Dairy was <span class="text-decoration-underline">certified organic</span> in 2005 and we became a part of Organic Valley Coop. Gerrit’s observation is, <strong>“The cows are healthy if they eat healthy grass. If you don’t have healthy grass, you don’t have healthy soil, and you can’t have healthy soil if you poison it year after year with chemicals”</strong>.<br/><br/>
-							Our family enjoys nature’s theater each day as we witness a vast array of diversity of life. It’s not uncommon to see owls, hawks, bald eagles, bats, coyotes, herons, ducks and geese. Thankfully we have yet to see a bear or cougar on our property!
-						</span>
-					</v-col>
-				</v-row>
-			</v-container>
-		</v-parallax>
+		<div style="max-height: 800px; background-color: #88A379">
+			<v-parallax src="../assets/cows.jpg" height="800">
+				<v-container>
+					<v-row justify="center">
+						<v-col cols="6" class="card-title-overlay pa-5" dark>
+							<span class="f-dancing h5-size"> Teaming Up With Nature </span> <br/>
+							<span class="f-roboto-slab h1-size">
+								Van Tol Dairy was <span class="text-decoration-underline">certified organic</span> in 2005 and we became a part of Organic Valley Coop. Gerrit’s observation is, <strong>“The cows are healthy if they eat healthy grass. If you don’t have healthy grass, you don’t have healthy soil, and you can’t have healthy soil if you poison it year after year with chemicals”</strong>.<br/><br/>
+								Our family enjoys nature’s theater each day as we witness a vast array of diversity of life. It’s not uncommon to see owls, hawks, bald eagles, bats, coyotes, herons, ducks and geese. Thankfully we have yet to see a bear or cougar on our property!
+							</span>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-parallax>
+		</div>
 		<v-container class="mt-5 mb-5">
 			<v-row class="screen-height" justify="center" align="center">
 				<v-col cols="9"> 
